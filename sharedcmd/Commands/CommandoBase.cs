fc6eaed074945f9cf46005d9d80e1f115dc45c87
simple rick <69311713+simpleZed@users.noreply.github.com>
2021-06-04 17:03:59 -0300

    Removes unecessary arguments list.

--- sharedcmd/Commands/CommandoBase.cs
+++ sharedcmd/Commands/CommandoBase.cs
@@ -14,15 +14,11 @@ namespace sharedcmd.Commands
     {
         protected readonly List<string> commands = new();
 
-        protected readonly List<T> arguments = new();
-
         private readonly ShellBase<T> shell;
 
         public string? Command => commands.FirstOrDefault();
 
         public string? Arguments => commands.Skip(1)
-                                            .Concat(arguments.Select(a => shell.BuildArgument(a))
-                                                             .Where(s => !string.IsNullOrWhiteSpace(s)))
                                             .Aggregate((x, y) => $"{x} {y}");
 
         protected CommandoBase(ShellBase<T> shell)
@@ -60,6 +56,7 @@ namespace sharedcmd.Commands
             return true;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public override bool TryGetIndex(GetIndexBinder binder, object[] indexes, out object result)
         {
             AddCommands(indexes.OfType<string>());
@@ -71,7 +68,7 @@ namespace sharedcmd.Commands
         public override bool TryInvoke(InvokeBinder binder, object[] args, out object result)
         {
             var parsedArguments = binder.ParseArguments<T>(args);
-            arguments.AddRange(parsedArguments);
+            AddCommands(parsedArguments.Select(a => a.ToString()));
             result = shell.Run(new RunOptions(this));
             return true;
         }
